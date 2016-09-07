001f1363c824b2ce1e67dd8a2d9c163063da8aae
Melissa Castine <m_castine@me.com>
2016-09-07 11:59:00 -0400

    Adding code to copy classes from Modifier ontology into a specified local domain ontology.

---
+++ src/main/java/copyModifierClasses.java
@@ -0,0 +1,11 @@
+import java.io.File;
+
+/**
+ * Created by melissa on 9/7/16.
+ */
+public class copyModifierClasses {
+
+    public static void main(String[] args){
+        File domainFile = new File(args[0]);
+    }
+}
